709e9599e040d17f98048486b9c43c38dd37cf0c
unknown <kozyr52@gmail.com>
2023-04-11 02:56:06 +0300

    fix

--- src/pages/EditTaskPage/EditTaskPage.tsx
+++ src/pages/EditTaskPage/EditTaskPage.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Typography } from '@mui/material';
+import { useParams } from 'react-router-dom';
 import { EditTaskForm } from 'modules/index';
 
 export function EditTaskPage() {
+  const { id } = useParams();
   return (
     <>
-      <Typography variant="h3" component="h2">
-        TODO LIST | EDIT TASK
+      <Typography variant="h4" component="h2">
+        TODO LIST | EDIT TASK {id}
       </Typography>
       <EditTaskForm />
     </>
